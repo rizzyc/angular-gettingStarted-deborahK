9d226b9b63c598cf06b552a218a307a211af49f0
Rizwan Choudhary <Rizwan.Choudhary@bondbrandloyalty.com>
2019-07-03 14:57:03 -0400

    building a product data service

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 @Component({
     selector: 'pm-products',
     templateUrl: './product-list.component.html',
@@ -19,31 +20,9 @@ export class ProductListComponent implements OnInit {
         this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
     }
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-        'productId': 1,
-        'productName': 'Leaf Rake',
-        'productCode': 'GDN-0011',
-        'releaseDate': 'March 19, 2016',
-        'description': 'Leaf rake with 48-inch wooden handle.',
-        'price': 19.95,
-        'starRating': 3.2,
-        'imageUrl': 'https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png'
-      },
-      {
-        'productId': 2,
-        'productName': 'Garden Cart',
-        'productCode': 'GDN-0023',
-        'releaseDate': 'March 18, 2016',
-        'description': '15 gallon capacity rolling garden cart',
-        'price': 32.99,
-        'starRating': 4.2,
-        'imageUrl': 'https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png'
-      }];
+    products: IProduct[] = [];
 
-      constructor() {
-          this.filteredProducts = this.products;
-          this.listFilter = 'cart';
+      constructor(private productService: ProductService) {
       }
 
       onRatingClicked(message: string): void {
@@ -60,7 +39,8 @@ export class ProductListComponent implements OnInit {
       }
 
       ngOnInit(): void {
-          console.log('In Oninit');
+        this.products = this.productService.getProducts();
+        this.filteredProducts = this.products;
       }
 
  }
